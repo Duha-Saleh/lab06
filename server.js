0e6cbcd27b448e596c790a5ff54cab2272d3f89d
Duha-Saleh <duhamahmoud096@gmail.com>
2020-07-28 15:25:56 +0300

    3

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 'use strict';
 
 
-const express =require('express');
-const cors =require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
-const app=express();
+const app = express();
 
-const PORT =process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 const superagent = require('superagent');
 
 
-app.listen(PORT,()=> {
+app.listen(PORT, () => {
     console.log('I am listning to port', PORT);
 });
 
@@ -24,8 +24,8 @@ app.listen(PORT,()=> {
 //     response.status(200).send(newLocation);
 
 // });
- 
- 
+
+
 // function Location(city,data){
 // this.search_query=city;
 // this.formatted_query=data[0].display_name;
@@ -55,8 +55,8 @@ app.listen(PORT,()=> {
 //     this.time=data2.valid_date;
 //     arr.push(this);
 //     }
-    
-    
+
+
 
 
 
@@ -87,86 +87,79 @@ function Location(city, data) {
 
 
 
-app.get('/weather', (request,response) =>{
-    var weather =request.query.forecast;
-    // var data2 =require('./data/weather.json');
-    let weatherKey=process.env.WEATHER_API_KEY;
-    let url='';
-
-    });
-
-    superagent.get(url)
-        .then(data => {
-            data2.data.forEach(element =>{
-        
-        var newWather = new Weather(element);
-        
-});
-
-    response.status(200).send(arr);
+app.get('/weather', (request, response) => {
+    var weather = request.query.forecast;
+    let weatherKey = process.env.WEATHER_API_KEY;
+    let url = 'https://api.weatherbit.io/v2.0/forecast/daily?lat=${38.123}&lon=${-78.543}&key=${weatherKey}';
 
 });
 
-let arr=[];
-function Weather(data2){
-    this.forcast=data2.weather.description;
-    this.time=data2.valid_date;
-    arr.push(this);
-    }
+superagent.get(url)
+    .then(data2 => {
+       data2.forEach(element => {
 
+            var newWather = new Weather(element);
 
-
-    app.get('/trial', (request,response) =>{
-        var trial =request.query.trial;
-        // var data2 =require('./data/weather.json');
-        let trialKey=process.env.WEATHER_API_KEY;
-        let url='';
-    
         });
-    
-        superagent.get(url)
-            .then(data => {
-                data2.data.forEach(element =>{
-            
-            var newTrial = new Trial(element);
-            
-    });
-    
+
         response.status(200).send(arr);
-    
+
     });
-    
-    let arr=[];
-    function Weather(data2){
-        this.forcast=data2.weather.description;
-        this.time=data2.valid_date;
-        arr.push(this);
-        }
-    
-    
 
-        
-        let arr2=[];
-
-    function Trail(dataOfTrails) {
-        this.name= dataOfTrails.name;
-        this.location = dataOfTrails.location;
-        this.length =dataOfTrails.length;
-        this.stars = dataOfTrails.stars;
-        this.star_votes = dataOfTrails.starVotes;
-        this.summary = dataOfTrails.summary;
-        this.trail_url = dataOfTrails.url;
-        this.conditions = dataOfTrails.conditionStatus;
-        arr2.push(this);
-
-    }
+let arr = [];
+function Weather(data2) {
+    this.forcast = data2.weather.description;
+    this.time = data2.valid_date;
+    arr.push(this);
+}
 
 
-    app.get('*', (req, res) => {
-        res.status(404).send('Page Not Found');
-    });
-    
-    app.use((error, req, res) => {
-        res.status(500).send('error');
-    });
-    
+
+app.get('/trial', (request, response) => {
+    var trial = request.query.trial;
+    let trialKey = process.env.TRIAL_API_KEY;
+let url='https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=${trialKey1}';
+
+
+
+
+});
+
+
+superagent.get(url).then(data3 => {
+        data3.forEach(element => {
+
+            var newTrial = new Trial(element);
+
+        });
+
+        response.status(200).send(arr);
+
+    });};
+
+
+
+
+let arr2 = [];
+
+function Trail(data3) {
+    this.name = data3.name;
+    this.location = data3.location;
+    this.length = data3.length;
+    this.stars = data3.stars;
+    this.star_votes = data3.starVotes;
+    this.summary = data3.summary;
+    this.trail_url = data3.url;
+    this.conditions = data3.conditionStatus;
+    arr2.push(this);
+
+}
+
+
+app.get('*', (req, res) => {
+    res.status(404).send('Page Not Found');
+});
+
+app.use((error, req, res) => {
+    res.status(500).send('error');
+});
