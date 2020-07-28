49dbf143a4caf8b3e04a6e34bb1053eb131dbf01
Duha-Saleh <duhamahmoud096@gmail.com>
2020-07-28 17:20:45 +0300

    4

--- server.js
+++ server.js
@@ -77,32 +77,37 @@ app.get('/location', (req, res) => {
         });
 
 });
-
+var latitudee= [];
+var longitudee= [] ;
 function Location(city, data) {
     this.search_query = city;
     this.formatted_query = data[0].display_name;
     this.latitude = data[0].lat;
     this.longitude = data[0].lon;
+    latitudee.push(this.latitude);
+    longitudee.push(this.longitude);
 }
-
 
 //localhost:3000/weather
 app.get('/weather', (request, response) => {
     var weather = request.query.forecast;
+    
     let weatherKey = process.env.WEATHER_API_KEY;
-    let url =`https://api.weatherbit.io/v2.0/forecast/daily?lat=${38.123}&lon=${-78.543}&key=${weatherKey}`;
+    let url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${latitudee}&lon=${longitudee}&key=${weatherKey}`;
 
     superagent.get(url)
-    .then(data2 => {
-       data2.forEach(element => {
+        .then(data2 => {
+            var dataOfweather=JSON.parse(data2.text).data;
 
-            var newWather = new Weather(element);
+            dataOfweather.forEach(element => {
+
+                var newWather = new Weather(element);
+
+            });
+
+            response.status(200).json(arr);
 
         });
-
-        response.status(200).send(arr);
-
-    });
 
 
 });
@@ -116,45 +121,50 @@ function Weather(data2) {
 
 
 
-// app.get('/trial', (request, response) => {
-//     var trial = request.query.trial;
-//     let trialKey = process.env.TRIAL_API_KEY;
-// let url='https://www.https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=${trialKey}';
+
+//localhost:3000/trial
+app.get('/trails', (request, response) => {
+    // var trial = request.query.trial;
+    let trialKey = process.env.TRIAL_API_KEY;
+    let url = `https://www.hikingproject.com/data/get-trails?lat=${latitudee}&lon=${longitudee}&maxDistance=100&key=${trialKey}`;
+
+
+    superagent.get(url).then(data3 =>  {
+        var dataOfTrial=JSON.parse(data3.text).trails;
+       
+        dataOfTrial.forEach(element => {
+    
+                var newTrial = new Trails(element);
+    
+            });
+    
+            response.status(200).json(arr2);
+    
+        });
+    
+
+
+});
 
 
 
 
 
+let arr2 = [];
+function Trails(data3) {
+    this.name = data3.name;
+    this.location = data3.location;
+    this.length = data3.length;
+    this.stars = data3.stars;
+    this.star_votes = data3.starVotes;
+    this.summary = data3.summary;
+    this.trail_url = data3.url;
+    this.conditions = data3.conditionStatus;
+this.condition_time=data3.conditionDate;
 
-// });
+    arr2.push(this);
 
-
-// superagent.get(url).then(data3 => {
-//         data3.forEach(element => {
-
-//             var newTrial = new Trial(element);
-
-//         });
-
-//         response.status(200).send(arr2);
-
-//     });
-
-
-
-// let arr2 = [];
-// function Trail(data3) {
-//     this.name = data3.name;
-//     this.location = data3.location;
-//     this.length = data3.length;
-//     this.stars = data3.stars;
-//     this.star_votes = data3.starVotes;
-//     this.summary = data3.summary;
-//     this.trail_url = data3.url;
-//     this.conditions = data3.conditionStatus;
-//     arr2.push(this);
-
-// }
+}
 
 
 app.get('*', (req, res) => {
